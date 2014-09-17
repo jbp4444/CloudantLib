afda98ccee3ff81d10041cc7c92ba284dd371b46
jbp4444 <jbp1@duke.edu>
2014-09-17 11:41:56 -0400

    trying (again) to get rid of password info

--- cloudantInfo.lua
+++ cloudantInfo.lua
@@ -1,8 +1,6 @@
 -- set some global vars
-cloudant_baseurl = "https://jbp4444.cloudant.com"
+cloudant_baseurl = "https://foo.cloudant.com"
 cloudant_database = "mydb"
-cloudant_username = "aungrongondownediftenect"
-cloudant_password = "HmfTOvhBibX40BJDm044hJap"
--- read-only:
---username = "hosellydnelloolughtonswe",
---password = "cA1HGLqxOHNyjgGJmwNIDBwH",
+cloudant_username = "crazycloudantusername"
+cloudant_password = "crazycloudantpassword"
+
